d5c55faffac2edac0a4392b938c2b6cd34f6c88c
amila <amilastbmmv@gmail.com>
2012-01-18 20:28:43 +0530

    third

---
+++ 100379T/build/classes/pkg100379t/Main.rs
@@ -0,0 +1,4 @@
+pkg100379t.Animal
+pkg100379t.Dog
+pkg100379t.Bird
+pkg100379t.Main
--- 100379T/src/pkg100379t/Main.java
+++ 100379T/src/pkg100379t/Main.java
@@ -1,19 +1,75 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package pkg100379t;
 
 /**
- *
  * @author Tharindu Amila
  */
+
+abstract class Animal{
+    private int numberOfLegs;
+    private String shout;
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+    public void drink(){
+        System.out.println("Animal is Drinking");
+    }
+    public void shout(){
+        System.out.println(shout);
+    }
+    public void setSound(String sound){
+        shout = sound;
+    }
+    public void setLegs(int leg){
+        numberOfLegs = leg;
+    }
+    public int getLegs(){
+        return numberOfLegs;
+    }
+    public abstract void move();
+}
+
+
+class Dog extends Animal{
+
+    Dog(){
+        this.setLegs(4);
+        this.setSound("Bow Bow");
+    }
+    @Override
+    public void move() {
+        System.out.println("The dog is running with its "+super.getLegs()+" legs");
+    }
+}
+
+
+class Bird extends Animal{
+
+    Bird(){
+        this.setLegs(2);
+        this.setSound("Chirp Chirp");
+    }
+    @Override
+    public void move() {
+        System.out.println("The Bird is flying");
+    }
+}
+
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Dog rover = new Dog();
+        Bird tutu = new Bird();
+        System.out.println("The Dog Class in action");
+        rover.shout();
+        rover.drink();
+        rover.eat();
+        rover.move();
+        System.out.println("\nThe Bird Class in action");
+        tutu.shout();
+        tutu.drink();
+        tutu.eat();
+        tutu.move();
+       
     }
 }
